32e021d69c52336b18a565e8003b86b6b44814cb
siima <173372546+SalmaFartah@users.noreply.github.com>
2026-05-06 10:10:08 +0100

    s

--- include/checkDirect.hpp
+++ include/checkDirect.hpp
@@ -2,6 +2,12 @@
 
 #include <string>
 #include <vector>
+#include <map>
+// regex is used to check if a piece of text matches a specific format or rule.
+enum tokenType
+{
+
+};
 
 typedef struct
 {
@@ -12,6 +18,8 @@ typedef struct
     bool autoindex; // on/off
     std::vector<std::string> index; // index index.php index.html;
     std::string upload_store; // path where the client post smth
+    std::string cgi_extension; // script extension: .php/.py/.pl
+    std::string cgi_pass; // the executer that will run the script
 }   locationConf;
 
 typedef struct
@@ -22,3 +30,8 @@ typedef struct
     size_t max_body_sz;
     std::vector<locationConf> locations;
 }   serverConf;
+
+
+void fillServer(std::vector<std::pair<tokenType, std::string>>, int&, std::vector<serverConf>&);
+
+void filllocation(std::vector<std::pair<tokenType, std::string>>, int&, std::vector<locationConf>&);
